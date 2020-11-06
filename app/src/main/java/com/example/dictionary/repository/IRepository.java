50a27d48612c3777e95ed4991741a0c30ec69f47
fariba <fariba.darvishyan1994@gmail.com>
2020-11-06 09:45:05 +0330

    -add repository for good structure create Interface include all method abstract implementation in DBRepository

---
+++ app/src/main/java/com/example/dictionary/repository/IRepository.java
@@ -0,0 +1,18 @@
+package com.example.dictionary.repository;
+
+import com.example.dictionary.model.DictionaryWord;
+
+import java.util.List;
+
+public interface IRepository {
+
+    void insertWord(DictionaryWord word);
+    void updateWord(DictionaryWord word);
+    void deleteWord(DictionaryWord word);
+    List<DictionaryWord> getWords();
+    DictionaryWord getWord(long inputId);
+    List<DictionaryWord> searchArabic(String searchValue);
+    List<DictionaryWord> searchEnglish(String searchValue);
+    List<DictionaryWord> searchFrench(String searchValue);
+    List<DictionaryWord> searchPersian(String searchValue);
+}
